a35870f3e10bd5be12ea454efbbda0fd6479d212
khurshed <hurshed@gmail.com>
2023-07-09 02:56:50 +0500

    Добавил решение task_6

--- task_6/Program.cs
+++ task_6/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число четным (делится ли оно без остатка).
+// 4 -> Да
+// -3 -> Нет
+// 7 -> Нет
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+if (number % 2 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
